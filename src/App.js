b828dbf87447cb4077b9b322e833d23978f5446b
Diego Hidalgo <Hidalgo.moreno.diego@gmail.com>
2024-12-27 15:48:20 +0100

    refactor: standardize import statements in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import NavBar  from './components/NavBar';
-import Home  from './components/Home';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contacts from './components/Contacts';
+import  NavBar  from './components/NavBar';
+import { Home } from './components/Home';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Contacts } from './components/Contacts';
 
 function App() {
   return (
