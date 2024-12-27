36bacb9d5f78dd17d84ac5aeaee442cb45e8ae4d
DiegoHidalgo02 <hidalgo.moreno.diego@gmail.com>
2024-12-27 15:15:13 +0100

    fix: replace React.StrictMode with Provider for Redux store integration

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode store={store}>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
